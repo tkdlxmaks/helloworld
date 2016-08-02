a60d0a18234b881cfb6f1ecab35d2053c99e93e0
kickscar <kickscar@gmail.com>
2016-08-02 12:58:25 +0900

    1st commit

---
+++ src/kr/ac/helloworld/servlet/HelloServlet.java
@@ -0,0 +1,28 @@
+package kr.ac.helloworld.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType( "text/html; charset=utf-8" );
+		PrintWriter out = response.getWriter();
+		out.println( "<h1>hello World</h1>" );
+		for( int i = 0; i < 10; i++ ) {
+			out.println( "<h2>안녕하세요.</h2>" );
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
